599cae18d5d329ff8ee1af7aba107a22e5e85ce5
redstringoffate <redstringoffate@naver.com>
2025-11-21 12:29:04 +0900

    Update purushartha.py

--- utils/purushartha.py
+++ utils/purushartha.py
@@ -1,10 +1,18 @@
+from utils.nakshatra_purushartha import NAKSHATRA_PURUSHARTHA
+
 PADA_PURUSHARTHA = {
     1: ("Dharma", "D"),
-    2: ("Artha", "A"),
-    3: ("Kama", "K"),
+    2: ("Artha",  "A"),
+    3: ("Kama",   "K"),
     4: ("Moksha", "M")
 }
 
 def get_purushartha(nk_name, nk_pada):
-    name, code = PADA_PURUSHARTHA[nk_pada]
-    return f"{name}-{code}"
+    # 1) Nakshatra-level Purushartha
+    nk_p = NAKSHATRA_PURUSHARTHA[nk_name]
+
+    # 2) Pada-level Purushartha code only
+    _, code = PADA_PURUSHARTHA[nk_pada]
+
+    # 3) Combine
+    return f"{nk_p}-{code}"
