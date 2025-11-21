03909ce425b41bcd443d349630c98db00531504c
redstringoffate <redstringoffate@naver.com>
2025-11-21 12:22:39 +0900

    Add files via upload

---
+++ utils/nakshatra_purushartha.py
@@ -0,0 +1,35 @@
+NAKSHATRA_PURUSHARTHA = {
+    "Ashwini":      "Dharma",
+    "Bharani":      "Artha",
+    "Krittika":     "Kama",
+    "Rohini":       "Moksha",
+
+    "Mrigashira":   "Dharma",
+    "Ardra":        "Artha",
+    "Punarvasu":    "Kama",
+    "Pushya":       "Moksha",
+
+    "Ashlesha":     "Dharma",
+    "Magha":        "Artha",
+    "Purva Phalguni": "Kama",
+    "Uttara Phalguni": "Moksha",
+
+    "Hasta":        "Dharma",
+    "Chitra":       "Artha",
+    "Swati":        "Kama",
+    "Vishakha":     "Moksha",
+
+    "Anuradha":     "Dharma",
+    "Jyeshtha":     "Artha",
+    "Mula":         "Kama",
+    "Purva Ashadha": "Moksha",
+
+    "Uttara Ashadha": "Dharma",
+    "Shravana":     "Artha",
+    "Dhanishta":    "Kama",
+    "Shatabhisha":  "Moksha",
+    
+    "Purva Bhadrapada": "Dharma",
+    "Uttara Bhadrapada": "Artha",
+    "Revati":   "Kama",
+}
